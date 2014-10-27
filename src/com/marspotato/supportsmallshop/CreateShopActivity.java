454d21fa7300bf3bf3f5919f5a66710193f01d30
TritonHo <triton.ho@marspotato.com>
2014-10-27 23:48:49 +0800

    complete create_shop.xml

--- src/com/marspotato/supportsmallshop/CreateShopActivity.java
+++ src/com/marspotato/supportsmallshop/CreateShopActivity.java
@@ -4,6 +4,8 @@ import org.joda.time.DateTime;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 public class CreateShopActivity extends Activity {
@@ -16,5 +18,10 @@ public class CreateShopActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.create_shop);
+		
+		Spinner spinner = (Spinner) findViewById(R.id.shop_type_spinner);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.shop_type_display_values , android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
 	}
 }
